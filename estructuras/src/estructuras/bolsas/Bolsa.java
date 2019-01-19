6fcf965ce5a3ced2f10d4df7c8148b2bd98e30a4
Yimmy Quispe Yujra <jin_maxtor@outlook.com>
2019-01-19 05:48:19 -0400

    Adicionado ADT Bolsa

---
+++ estructuras/src/estructuras/bolsas/Bolsa.java
@@ -0,0 +1,21 @@
+/*
+ * -------------------------------------------------------------------------
+ * Bolsa.java
+ * Descripcion: Interface utiliza para representar la abstrwcción đe una
+ * bolsa que contiene cualquier tipo de elementos.
+ * Estructuras de Datos en Java  ---  Yimmy Quispe Yujra, Santa Cruz - 2019
+ * -------------------------------------------------------------------------
+ */
+package estructuras.bolsas;
+
+/**
+ * Interface que representa la abstracción de una bolsa.
+ * Una bolsa es un conjunto finito de elementos que no tienen un orden particular,
+ * una bolsa puede contener elementos repetidos.
+ *
+ * @author Yimmy Quispe Yujra
+ * @version 1.0
+ */
+public interface Bolsa<T> {
+	
+}
